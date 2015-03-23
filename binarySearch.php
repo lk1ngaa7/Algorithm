96f8f803157edf253ccf0b8699e14a7aa0fa8115
lkinga7 <lk1ngaa7@gmail.com>
2015-03-23 10:52:14 +0800

    /*   数组 A 中任意两个相邻元素大小相差 1，现给定这样的数组 A 和目标整数 t， 找出 t 在数组 A 中的位置 */

--- binarySearch.php
+++ binarySearch.php
@@ -1,5 +1,17 @@
 <?php
+/*
+  数组 A 中任意两个相邻元素大小相差 1，现给定这样的数组 A 和目标整数 t，
+找出 t 在数组 A 中的位置
+*/
   function findInArray($array,$target){
+	  $index = $array[0] - $target;
+	  $index = $index > 0? $index: -$index;
+	  if($index > count($array))
+		  return -1;
+	  if($array[$index] == $target)
+	    return $index;
+	  else 
+		return -1; 
 	  
 	  
   }
@@ -25,11 +37,14 @@
 	   $i = 0;
 	   $array = array();
 	   for(;$i<=1000;$i++){
-		   $array[] = $i;
+		   $array[] = $i+0.6;
 		}
-	   $target = 908;
+	   $target = 908.6;
 	   $index = binarySearch($array,$target);
-	   echo ' target-----> '.$target.' is---> '.$index.' and the value is: '.$array[$index];
+	   $index1 = findInArray($array,$target);
+	   
+	   echo ' target-----> '.$target.' is---> '.$index;
+	   echo ' target-----> '.$target.' is---> '.$index1;
 	   
    }
    main();
